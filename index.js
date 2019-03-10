1c05d6fa90a8107532c8efa9057f08267cfa3b23
shivanmustafa <you@example.comshivanmustafa88@gmail.com>
2019-03-10 11:48:54 +0300

    this is project 3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+function palindrome(str) {
+    // Good luck!
+    return true;
+  }
+  
+  function palindrome(str){
+  str=str.replace(/[\W_]/g,"");
+  var reverseStr=str. toLowerCase().split("").reverse().join("");
+  if(str.toLowerCase === reverseStr)
+  {
+  
+  
+    return true;
+    }
+    else{
+      return false;
+    }
+  }
+  
+  palindrome("eye");
